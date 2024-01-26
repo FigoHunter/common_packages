f761ab17d417480c983f1de81d8c680af0cb384c
yifei <zhaoyifei19950308@hotmail.com>
2024-01-26 19:24:21 +0800

    subprocess

--- subprocess/__init__.py
+++ subprocess/__init__.py
@@ -1,28 +1,27 @@
 import subprocess
 import time
+import threading
 
-def run_command(command, callback=None):
+def run_command(command):
     process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
 
-    while True:
-        output = process.stdout.readline()
-        error = process.stderr.readline()
+    def stdout_callback(process, s):
+        print('Stdout: ' + s)
+    def stderr_callback(process, s):
+        print('Stderr: ' + s)
 
-        if process.poll() is not None and not output and not error:
-            break
+    stdout_thread = threading.Thread(target=_read_stream, args=(process.stdout, process, stdout_callback))
+    stderr_thread = threading.Thread(target=_read_stream, args=(process.stderr, process, stderr_callback))
 
-        if output:
-            print(f"Output: {output.strip()}")
-            if callback is not None:
-                callback(process, output.strip())
-        if error:
-            print(f"Error: {error.strip()}")
+    stdout_thread.start()
+    stderr_thread.start()
 
-        time.sleep(0.1)
+    stdout_thread.join()
+    stderr_thread.join()
 
     return process.returncode
 
-def run_command_iterator(command, callback=None):
+def run_command_iterator(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
 
     while True:
@@ -34,11 +33,17 @@ def run_command_iterator(command, callback=None):
 
         if output:
             yield f"Output: {output.strip()}"
-            if callback is not None:
-                callback(process, output.strip())
         if error:
             yield f"Error: {error.strip()}"
 
         time.sleep(0.1)
 
     yield f"Return: {process.returncode}" 
+
+
+def _read_stream(stream, process, callback=None):
+    for line in iter(stream.readline, b''):
+        if not line:
+            return
+        if callback is not None:
+            callback(process, line.strip())
