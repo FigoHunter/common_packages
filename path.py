d5fc9b9f715da5d114dc97dad37c685b3caa2540
zhaoyifei <zhaoyifei19950308@hotmail.com>
2024-08-21 06:58:30 +0000

    path

--- path.py
+++ path.py
@@ -6,7 +6,7 @@ if not WORKSPACE_HOME:
     WORKSPACE_HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..'))
 DATA_PATH = os.path.join(WORKSPACE_HOME, 'data')
 ASSET_PATH = os.path.join(WORKSPACE_HOME, 'assets')
-STARTUP_PATH=os.path.join(WORKSPACE_HOME,"startup")
+STARTUP_PATH=os.path.join(WORKSPACE_HOME,"blender_startup")
 
 
 def set_workspace_home(path):
