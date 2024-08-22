fa44563001d21b1b7415d7082a8257a07ec21165
zhaoyifei <zhaoyifei19950308@hotmail.com>
2024-08-22 07:20:10 +0000

    import

--- math/__init__.py
+++ math/__init__.py
@@ -1,6 +1,6 @@
-from . import matrix
-from . import mesh
-from . import vector
-from . import halfedge
-from . import transform_np
-from . import transform_tensor
+# from . import matrix
+# from . import mesh
+# from . import vector
+# from . import halfedge
+# from . import transform_np
+# from . import transform_tensor
--- module.py
+++ module.py
@@ -1,4 +1,6 @@
 import importlib
+import importlib.util
+import sys
 
 def module_exists(module_name):
     try:
@@ -6,3 +8,13 @@ def module_exists(module_name):
         return True
     except ImportError:
         return False
+    
+def runtime_import(path):
+    import os
+    module_name = os.path.basename(path)
+    init_py = os.path.join(path, '__init__.py')
+    spec = importlib.util.spec_from_file_location(module_name, init_py)
+    foo = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = foo
+    spec.loader.exec_module(foo)
+    return foo
