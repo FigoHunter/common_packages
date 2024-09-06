9cc245e2e702a5b5619a936ced0fc598d1462ae3
FigoHunter <zhaoyifei19950308@hotmail.com>
2024-09-06 14:52:14 +0800

    ignore

---
+++ packages/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='common_packages',
+    version='0.1.0',
+    packages=find_packages(),
+    license='MIT',
+    author='figo',
+    description='figo',
+    install_requires=[],
+    tests_require=[]
+)
